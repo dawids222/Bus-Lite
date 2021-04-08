3190c47cacf38c3bcd44dc3dc430a4bbe99a124f
dgrajewski <dawids115@gmail.com>
2021-04-08 23:06:00 +0200

    Added Examples class;

---
+++ Console-Playground/Examples.cs
@@ -0,0 +1,62 @@
+﻿using Bus_Lite;
+
+namespace Console_Playground
+{
+    class Examples
+    {
+        /// <summary>
+        /// creting an event bus
+        /// </summary>
+        public static EventBus CreateEventBusExample()
+        {
+            var eventBus = new EventBus();
+            return eventBus;
+        }
+
+        /// <summary>
+        /// subscribing to an event
+        /// first argument is an owner (almost always 'this')
+        /// second argument is a callback function, where '@event' is 'string'
+        /// method retuns token whitch is used to unsubscribing
+        /// </summary>
+        public SubscriptionToken SubscribeListenerToEventBusExample()
+        {
+            var eventBus = CreateEventBusExample();
+            var token = eventBus.Subscribe<string>(this, (@event) => { });
+            return token;
+        }
+
+        /// <summary>
+        /// unsubscribing listener given by token
+        /// </summary>
+        public void UnsubscribeListenerFromEventBusByTokenExample()
+        {
+            var eventBus = CreateEventBusExample();
+            var token = eventBus.Subscribe<string>(this, (@event) => { /* implementation */ });
+            eventBus.Unsubscribe(token);
+        }
+
+        /// <summary>
+        /// unsubscribing all owner's listeners
+        /// where owner is not 'SubscriptionToken' (almost always 'this')
+        /// </summary>
+        public void UnsubscribeListenerFromEventBusByOwnerExample()
+        {
+            var eventBus = CreateEventBusExample();
+            eventBus.Subscribe<string>(this, (@event) => { /* implementation */ });
+            eventBus.Subscribe<int>(this, (@event) => { /* implementation */ });
+            eventBus.Unsubscribe(this);
+        }
+
+        /// <summary>
+        /// pushing event
+        /// all required listeners will be notified
+        /// </summary>
+        public void PushEventToAllListenersExample()
+        {
+            var eventBus = CreateEventBusExample();
+            eventBus.Subscribe<string>(this, (@event) => { /* implementation */ });
+            eventBus.Push("example string");
+        }
+    }
+}
